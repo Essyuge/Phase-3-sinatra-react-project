b92b6fa853f1c27b057bce4366386555724034e9
Essyuge <ugeesther@gmail.com>
2022-11-14 23:20:10 +0300

    order all rentals

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,7 @@
 class RentalsController < ApplicationController
     set :default_content_type, 'application/json'
+
+    get "/rentals" do
+        #binding.pry
+       serialize(Rental.all.order("id ASC"))
+    end
