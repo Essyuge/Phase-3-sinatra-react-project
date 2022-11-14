595d4d5e5b7407f88ed02fa5106f95b4e464419c
Essyuge <ugeesther@gmail.com>
2022-11-14 23:26:30 +0300

    setting up logging functionalities

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -3,3 +3,15 @@ require "pry"
 class OwnersController < ApplicationController
     
 set :default_content_type, 'application/json'
+
+post "/signup" do
+    owner=Owner.new(params)
+    if owner.name.empty?||owner.email.empty? ||owner.tel.blank?||owner.password.empty?||Owner.find_by_email(params[:email])||Owner.find_by_name(params[:name])
+       status 401
+       { errors: "something went wrong" }.to_json
+    else
+     owner.save
+     owner.to_json
+    end 
+   end
+   
